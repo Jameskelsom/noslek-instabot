6458a9735e9269121c2b72f83a7e1f94a61a701e
James Kelson <45362392+Jameskelsom@users.noreply.github.com>
2020-01-13 19:45:32 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='noslek-instabot',
+    version='0.0.0',
+    description='A bot to follow people tha is a potentially follower to your \
+        profile and unfollows who does\'t follows you on instagram.',
+    author_email='kelsomfire@hotmail.com',
+    packages=[
+        'noslek_instabot_tutorial',
+        ],
+    install_requires=[
+
+    ],
+)
